1648b50a46825da9137c574e13e42870429521a5
Aryaman <aryamanabhi@gmail.com>
2025-07-15 21:47:54 +0530

    PaymentSummary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import {renderPaymentSummary} from './paymentSummary.js';
 
 export function renderOrderSummary() {
 
@@ -97,11 +98,10 @@ export function renderOrderSummary() {
       link.addEventListener('click',() => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
-
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
         container.remove();
-
         updateCartQuantity();
+        renderPaymentSummary();
       });
     });
 
@@ -140,6 +140,7 @@ export function renderOrderSummary() {
         const quantityLabel = document.querySelector(`.js-quantity-label-${productId}`);
         quantityLabel.innerHTML = newQuantity;
         updateCartQuantity();
+        renderPaymentSummary();
       });
     });
 
@@ -149,6 +150,7 @@ export function renderOrderSummary() {
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       });
     });
 }
