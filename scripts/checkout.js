668bccb3056fda93a54984489ab5c1a676971c3f
Aryaman <aryamanabhi@gmail.com>
2025-07-23 19:53:00 +0530

    XHR to fetch in Products

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,15 +1,11 @@
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
-import {loadProducts} from '../data/products.js';
+import {loadProductFetch} from '../data/products.js';
 import {loadCart} from '../data/cart.js';
 
 Promise.all([
-  new Promise((resolve) => {
-    loadProducts(() => {
-      resolve();
-    });
-  }),
+  loadProductFetch(),
   new Promise((resolve) => {
     loadCart(() => {
       resolve();
